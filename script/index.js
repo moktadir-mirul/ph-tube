7eb76d4c41ea02e795d35c5f25b951a03a37c0b4
moktadir-mirul <moktadir.mirul@gmail.com>
2025-03-12 17:37:31 +0600

    ongoing

--- script/index.js
+++ script/index.js
@@ -19,5 +19,43 @@ const loadButtons = (categories) => {
     }
     
 }
+const fetchAllVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => loadVideos(data.videos))
+}
+
+const loadVideos = (videos) => {
+    const videoContainer = document.getElementById('video-container');
+    videos.forEach(video => {
+        const div = document.createElement('div');
+        div.innerHTML =`
+                    <div class="card bg-base-100 space-y-4 rounded-lg">
+                <figure>
+                  <img
+                    src="${video.thumbnail}"
+                    alt="Shoes" />
+                </figure>
+                <div class="flex gap-3">
+                    <div class="avatar">
+                        <div class="w-12 h-12 rounded-full">
+                          <img src="https://i.ibb.co/D9wWRM6/olivia.jpg" />
+                        </div>
+                    </div>
+                    <div class="">
+                        <h2 class="card-title text-xl">Shape of You</h2>
+                        <div>
+                            <p class="text-gray-500 text-lg">Awlad Hossain</p>
+                            <p class="text-gray-500 text-base">2.1 views</p>
+                        </div>
+                    </div>
+                
+                </div>
+              </div>
+        `
+    });
+}
+fetchAllVideos();
 fetchCategories();
 loadButtons();
+loadVideos();
